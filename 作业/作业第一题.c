31e265379658d08328c0ebdebf2290f57e541b42
Ratatouille <307338947@qq.com>
2019-03-28 10:02:08 +0800

    第二天的作业

---
+++ 作业/作业第一题.c
@@ -0,0 +1,70 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+
+
+int get_string_length(char* str)
+{
+	int length = 0;
+	char * p = str;
+	while (*p++)
+	{
+		length++;
+	}
+	return length;
+}
+
+
+char * my_strstr(const char* heystack, const char* needle)
+{
+	char* p = heystack;
+	char* q = needle;
+	int len = get_string_length(needle);
+	int len_heystack = get_string_length(heystack);
+	int compared_length = 1;
+	//printf("Here is inside my_strstr and the length of needle is %d \n", len);
+
+	while (*p++)
+	{
+		len_heystack--;
+		if (len_heystack < len-1)
+		{
+			//printf("string not found!\n");
+			return NULL;
+		}
+		if (*(p) == *(q))
+		{
+			for (int i = 1; i < len; i++)
+			{
+				if(*(p + i) == *(q + i))
+				{
+					//printf("No. %d character is same %c \n",i, *(p+i));
+					compared_length++;
+				}
+				else
+				{
+					break;
+				}
+			}
+			if (compared_length == len)
+			{
+				//printf("found \n");
+				return p;
+			}
+		}
+	}
+
+}
+
+
+int main()
+{
+	char* str = "this is heysyack,and here is a needle inside";
+	char* test_str = "12345";
+	printf("%d\n", get_string_length(test_str));
+	char* needle = "needle";
+
+	char* p = my_strstr(str, needle);
+	printf("%s\n", p);
+
+	return 0;
+}
---
+++ 作业第二题/作业第二题.c
@@ -0,0 +1,116 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+int getKeyByValue(char *keyvaluebuf, char *keybuf, char *valuebuf, int * valuebuflen);
+int removeBlankFromHeadAndRear(char** str);
+
+
+int main(void)
+{
+	char buf[128] = { 0 };
+	int len = 0;
+
+	/* 在"key1 =    valude1"中查找"key1"匹配的字符串，
+	   如果找到，获取=后面的字符串，而且去掉多余空格
+	*/
+	int ret = getKeyByValue("key1 =    valude1   ", "key1", buf, &len);
+	//调用函数后：buf="valude1", len=7
+	printf("-->%s<--%d__%d", buf, len, ret);
+
+	return 0;
+}
+
+int getKeyByValue(const char *keyvaluebuf,const char *keybuf, char *valuebuf, int * valuebuflen)
+{
+	int key_length_gross = 0, value_length_gross = 0, key_length_net = 0, value_length_net = 0;
+	char* p_equal = keyvaluebuf;
+
+	while (*(p_equal++) != '=')
+	{
+		key_length_gross++;
+	}
+
+	char* tmp = p_equal;
+
+	while (*p_equal++)
+	{
+		value_length_gross++;
+	}
+	//printf("%d--%d--\n", key_length_gross, value_length_gross);
+
+	char* key = (char*)malloc(key_length_gross+1);
+	char* value = (char*)malloc(value_length_gross+1);
+
+	memcpy(key, keyvaluebuf, key_length_gross);
+	memcpy(value, tmp, value_length_gross);
+
+	key[key_length_gross] = 0;
+	value[value_length_gross] = 0;
+
+	//printf("--->%s<---\n", key);
+	//printf("--->%s<---\n", value);
+
+	key_length_net = removeBlankFromHeadAndRear(&key);
+	value_length_net = removeBlankFromHeadAndRear(&value);
+
+	//printf("%d__%d__\n",key_length_net,value_length_net);
+	//printf("--->%s<---\n--->%s<---\n", key, value);
+
+	
+	strcpy(valuebuf, value);
+	*valuebuflen = value_length_net;
+
+	free(key);
+	free(value);
+
+	if (strcmp(key, keybuf) == 0)
+		return 0;
+	else
+		return -1;
+}
+
+
+int removeBlankFromHeadAndRear(char** str)
+{
+	unsigned int len = strlen(*str);
+	char* head = *str;
+	char* rear = *str + len -1;
+	while (head < rear)
+	{
+		if (*head == ' ')
+			head++;
+		else if (*rear == ' ')
+			rear--;
+		else
+			break;
+	}
+	int length_net = rear - head;
+	head[length_net+1] = 0;
+	//printf("--->inside remove length net %d\n",length_net+1);
+	strcpy(*str, head);
+	return length_net + 1;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
